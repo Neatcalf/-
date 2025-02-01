a0c2b3b707756073f38c2348fc9d026da32f1b07
Neatcalf <148421000+Neatcalf@users.noreply.github.com>
2025-02-01 10:20:10 +0500

    otkrirka.cs

--- Открытка/Program.cs
+++ Открытка/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace pozdravitelnaya_otkritka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+
+            Console.WriteLine("Введите имя получателя: ");
+
+            data = Console.ReadLine();
+
+            Console.WriteLine(data + ", поздравляю тебя с твоим днём рождения, желаю всего наилучшего.");
+            Console.WriteLine("Желаю, чтобы каждый день был наполнен яркими эмоциями и впечатлениями.");
+            Console.WriteLine("Пусть жизнь радует приятными сюрпризами, новыми открытиями и интересными встречами.");
+            Console.WriteLine("Будь здоров и полон сил для новых свершений!");
+            Console.WriteLine("Надеюсь, что этот год принесёт тебе массу положительных эмоций и ярких моментов");
+            Console.WriteLine("Отметь свой праздник так, как ты мечтаешь!");
+
+
+
+
+            Console.WriteLine("от Имиля, для " + data + "!!");
+        }
+    }
+}
